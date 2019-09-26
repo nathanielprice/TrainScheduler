0be2e455612f6120aa6819d17c57d2c5822e5bdc
Nate Price <nate.e.price@gmail.com>
2019-09-26 15:26:01 -0500

    it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,11 @@ var database = firebase.database();
 var name;
 var destination;
 var firstTrain;
-var frequency;
+var frequency = 0;
 
 $("button").on("click", function (event) {
 
-    event.PreventDefault();
+    event.preventDefault();
 
     // Storage of the new train data ---------------------------------
     name = $("#train-name").val().trim();
@@ -39,6 +39,42 @@ $("button").on("click", function (event) {
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-})
+    $("form")[0].reset();
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // store everything into a variable ----------------------------------------
+    var nextArr;
+    var minAway;
+    var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm)").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTrainNew), "minuets");
+    var remainder = diffTime % childSnapshot.val().frequency;
+    var minAway = childSnapshot.val().frequency - remainder;
+    var nextTrain = moment().add(minAway, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
+
+    $("#add-row").append("<tr><td>" + childSnapshot.val().name +
+        "</td><td>" + childSnapshot.val().destination +
+        "</td><td>" + childSnapshot.val().frequency +
+        "</td><td>" + nextTrain + 
+        "</td><td>" + minAway + "</td></tr>");
+
+            // Handle the errors
+        }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+    });
+
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").html(snapshot.val().name);
+        $("#email-display").html(snapshot.val().email);
+        $("#age-display").html(snapshot.val().age);
+        $("#comment-display").html(snapshot.val().comment);
+    });
+
+
+
 
 
